a7af52aa8f43b8027aa7f1b88d093aba2aba6678
Alberto Mendoza Rodriguez <alu0101217741@ull.edu.es>
2021-12-17 20:50:31 +0000

    Sign In tests failed

--- src/app/pages/my-account/components/signin/signin.component.spec.ts
+++ src/app/pages/my-account/components/signin/signin.component.spec.ts
@@ -1,4 +1,7 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { SigninComponent } from './signin.component';
 
@@ -8,6 +11,7 @@ describe('SigninComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [ReactiveFormsModule, RouterModule.forRoot([]), HttpClientModule],
       declarations: [ SigninComponent ]
     })
     .compileComponents();
@@ -19,7 +23,51 @@ describe('SigninComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('the component should created correctly.', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('the component should have a form.', () => {
+    expect(component.signInForm).toBeTruthy();
+  });
+
+  it('toggleFieldTextType() must change the value of the variable fieldTextType.', () => {
+    component.toggleFieldTextType();
+    expect(component.fieldTextType).toBeTruthy();
+  });
+
+  it('the component should have a returnUrl variable.', () => {
+    expect(component.returnUrl).toEqual('/');
+  });
+
+  it('there must be a method called onSubmit.', () => {
+    let onsubmit = spyOn(component, 'onSubmit')
+    component.onSubmit();
+    expect(onsubmit).toHaveBeenCalled();
+  });
+
+  it('method f should return correct values.', () => {
+    component.signInForm.controls["email"].setValue("test");
+    component.signInForm.controls["password"].setValue("test");
+    let form = component.f;
+    expect(form["email"].value).toEqual("test");
+    expect(form["password"].value).toEqual("test");
+  });
+
+  it('the call to the onSubmit method with correct credentials works correctly.', () => {
+    component.signInForm.controls["email"].setValue("test");
+    component.signInForm.controls["password"].setValue("test");
+    component.onSubmit();
+    expect(component.submitted).toBeTruthy();
+    expect(component.loading).toBeTruthy();
+  });
+
+  it('the call the onSubmit method with wrong credentials causes an error.', () => {
+    component.signInForm.controls["email"].setValue("");
+    component.signInForm.controls["password"].setValue("");
+    component.onSubmit();
+    expect(component.submitted).toBeTruthy();
+    expect(component.error).toEqual("");
+    expect(component.loading).toBeFalsy();
   });
 });
