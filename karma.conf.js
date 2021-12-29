c72eb92862fbf6bcba19019d43d0dc29ebf99d58
Alberto Mendoza Rodriguez <alu0101217741@ull.edu.es>
2021-12-29 16:02:45 +0000

    Added more settings for Travis CI

--- karma.conf.js
+++ karma.conf.js
@@ -33,17 +33,17 @@ module.exports = function (config) {
       ]
     },
     reporters: ['progress', 'kjhtml'],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['Chrome', 'ChromeHeadless', 'ChromeHeadlessCI'],
     customLaunchers: {
-      ChromeHeadlessCI: {
+      ChromeHeadlessNoSandbox: {
         base: 'ChromeHeadless',
         flags: ['--no-sandbox']
       }
     },
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['Chrome'],
     singleRun: false,
     restartOnFileChange: true
   });
