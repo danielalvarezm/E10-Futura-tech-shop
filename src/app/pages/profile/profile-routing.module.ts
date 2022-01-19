5a9c561d153fc643a9c93bde2cda31e3cbfe2cd9
Jeff Pérez Frade <93325379+jeffperezfrade@users.noreply.github.com>
2022-01-19 19:19:01 +0000

    Add files via upload
    Cuenta de Usuario

--- src/app/pages/profile/profile-routing.module.ts
+++ src/app/pages/profile/profile-routing.module.ts
@@ -2,7 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProfileComponent } from './profile.component';
 
-const routes: Routes = [{ path: '', component: ProfileComponent }];
+const routes: Routes = [
+  { path: '', component: ProfileComponent },
+  { path: 'personal-details', loadChildren: () => import('./personal-details/personal-details.module').then(m => m.PersonalDetailsModule) }
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -20,6 +20,22 @@ export class ProfileComponent {
       this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
    }
 
+  navPedidos(){
+    this.router.navigate(['/#']);
+  }
+
+  navFavoritos(){
+    this.router.navigate(['/#']);
+  }
+
+  navMonedero(){
+    this.router.navigate(['/#']);
+  }
+
+  navDatosPersonales(){
+    this.router.navigate(['/#']);
+  }
+
   logout() {
     this.authenticationService.logout();
     this.router.navigate(['/mi-cuenta']);
