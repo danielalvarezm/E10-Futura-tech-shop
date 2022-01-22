d8f91afde1c0e15f457d3b56074a275a7401dd9e
Alberto Mendoza Rodriguez <alu0101217741@ull.edu.es>
2022-01-22 20:03:07 +0000

    Updating routes

--- src/app/pages/profile/profile-routing.module.ts
+++ src/app/pages/profile/profile-routing.module.ts
@@ -3,8 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProfileComponent } from './profile.component';
 
 const routes: Routes = [
-  { path: '', component: ProfileComponent },
-  { path: 'personal-details', loadChildren: () => import('./personal-details/personal-details.module').then(m => m.PersonalDetailsModule) }
+  { path: '', component: ProfileComponent }
+  // { path: 'personal-details', loadChildren: () => import('./personal-details/personal-details.module').then(m => m.PersonalDetailsModule) }
 
 ];
 
