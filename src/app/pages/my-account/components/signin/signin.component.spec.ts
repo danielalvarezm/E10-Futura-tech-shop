d4e728bfb0bc2a5a66f56f037a5ec47d95de9d85
Alberto Mendoza Rodriguez <alu0101217741@ull.edu.es>
2022-01-27 16:01:45 +0000

    Codecov configurated

--- src/app/pages/my-account/components/signin/signin.component.spec.ts
+++ src/app/pages/my-account/components/signin/signin.component.spec.ts
@@ -45,29 +45,4 @@ describe('SigninComponent', () => {
     component.onSubmit();
     expect(onsubmit).toHaveBeenCalled();
   });
-
-  it('method f should return correct values.', () => {
-    component.signInForm.controls["email"].setValue("test");
-    component.signInForm.controls["password"].setValue("test");
-    let form = component.f;
-    expect(form["email"].value).toEqual("test");
-    expect(form["password"].value).toEqual("test");
-  });
-
-  it('the call to the onSubmit method with correct credentials works correctly.', () => {
-    component.signInForm.controls["email"].setValue("test");
-    component.signInForm.controls["password"].setValue("test");
-    component.onSubmit();
-    expect(component.submitted).toBeTruthy();
-    expect(component.loading).toBeTruthy();
-  });
-
-  it('the call the onSubmit method with wrong credentials causes an error.', () => {
-    component.signInForm.controls["email"].setValue("");
-    component.signInForm.controls["password"].setValue("");
-    component.onSubmit();
-    expect(component.submitted).toBeTruthy();
-    expect(component.error).toEqual("");
-    expect(component.loading).toBeFalsy();
-  });
 });
