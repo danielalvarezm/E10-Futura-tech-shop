0b92c06956ccf0f5b221896c8f0d262d8cc3afea
Jeff Pérez Frade <93325379+jeffperezfrade@users.noreply.github.com>
2022-01-29 15:53:17 +0000

    Add files via upload

---
+++ src/app/pages/main-page/components/products-interest/products-interest.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-products-interest',
+    templateUrl: './products-interest.component.html',
+    styleUrls: ['./products-interest.component.css']
+  })
+
+  export class ProductsInterestComponent {
+      constructor() {}
+      
+  }
