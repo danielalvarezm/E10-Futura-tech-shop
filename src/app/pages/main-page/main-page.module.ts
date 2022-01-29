bc29b384b6ef865f15da1d99ff36637965dbe201
Jeff Pérez Frade <93325379+jeffperezfrade@users.noreply.github.com>
2022-01-29 15:56:50 +0000

    Add files via upload

---
+++ src/app/pages/main-page/main-page.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { MainPageComponent } from './main-page.component';
+import { MainPageRoutingModule } from './main-page-routing.module';
+
+import { SliderComponent } from './components/slider/slider.component';
+import { RelevantCategoriesComponent } from './components/relevant-categories/relevant-categories.component';
+import { ProductsInterestComponent } from './components/products-interest/products-interest.component';
+import { NewsComponent } from './components/news/news.component';
+
+@NgModule({
+  declarations: [
+    MainPageComponent,
+    SliderComponent,
+    RelevantCategoriesComponent,
+    ProductsInterestComponent,
+    NewsComponent
+  ],
+  imports: [
+    CommonModule,
+    MainPageRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
+  ]
+})
+export class MainPageModule { }
