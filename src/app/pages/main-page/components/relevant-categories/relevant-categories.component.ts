1983f785fbeb945ef602d4a1e026df7ae0734500
Jeff Pérez Frade <93325379+jeffperezfrade@users.noreply.github.com>
2022-01-29 15:58:24 +0000

    Add files via upload

---
+++ src/app/pages/main-page/components/relevant-categories/relevant-categories.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-relevant-categories',
+    templateUrl: './relevant-categories.component.html',
+    styleUrls: ['./relevant-categories.component.css']
+  })
+
+  export class RelevantCategoriesComponent {
+      constructor() {}
+      
+  }
