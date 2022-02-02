e9f9131a1716331dece7114bb5d3810df4cb7abc
Jeff Pérez Frade <93325379+jeffperezfrade@users.noreply.github.com>
2022-02-02 11:34:40 +0000

    Add files via upload
    Actualización de rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './_helpers';
 
+
 const routes: Routes = [
   //{ path: '', redirectTo: '', pathMatch: 'full' },
-    { path: '', loadChildren: () => import('./pages/main-page/main-page.module').then(m => m.MainPageModule) },
+  //{ path: 'profile/access-accounts', loadChildren: () => import('./pages/profile/access-accounts/access-accounts.module').then(m => m.AccessAccountsModule) },
+  //{ path: 'profile/personal-details', component: PersonalDetailsComponent },
+  //{ path: 'profile/access-accounts', component: AccessAccountsComponent },
+  //{ path: 'profile', component: ProfileComponent },
+
   { path: 'mi-cuenta', loadChildren: () => import('./pages/my-account/my-account.module').then(m => m.MyAccountModule) },
   { path: 'carrito', loadChildren: () => import('./pages/shopping-cart/shopping-cart.module').then(m => m.ShoppingCartModule) },
   { path: 'profile', loadChildren: () => import('./pages/profile/profile.module').then(m => m.ProfileModule), canActivate: [AuthGuard] },
+  { path: '', loadChildren: () => import('./pages/main-page/main-page.module').then(m => m.MainPageModule) },
   { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
 
