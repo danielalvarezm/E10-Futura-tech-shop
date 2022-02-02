c7da5498628cda3e710c514fa345f7b711902648
Jeff Pérez Frade <93325379+jeffperezfrade@users.noreply.github.com>
2022-02-02 11:35:44 +0000

    Add files via upload
    Actualización de rutas

--- src/app/pages/shopping-cart/shopping-cart-routing.module.ts
+++ src/app/pages/shopping-cart/shopping-cart-routing.module.ts
@@ -1,14 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ShoppingCartComponent } from './shopping-cart.component';
+import { ConfirmacionComponent } from './tramitar-compra/confirmacion/confirmacion.component';
+import { DatosPersonalesComponent } from './tramitar-compra/datos-personales/datos-personales.component';
+import { EnvioComponent } from './tramitar-compra/envio/envio.component';
+import { PagoComponent } from './tramitar-compra/pago/pago.component';
 
 const routes: Routes = [
-  { path: '', component: ShoppingCartComponent },
-  { path: 'envio', loadChildren: () => import('./tramitar-compra/envio/envio.module').then(m => m.EnvioModule) },
-  { path: 'datos-personales', loadChildren: () => import('./tramitar-compra/datos-personales/datos-personales.module').then(m => m.DatosPersonalesModule) },
-  { path: 'pago', loadChildren: () => import('./tramitar-compra/pago/pago.module').then(m => m.PagoModule) },
-  { path: 'confirmacion', loadChildren: () => import('./tramitar-compra/confirmacion/confirmacion.module').then(m => m.ConfirmacionModule) },
-  { path: '**', redirectTo: '', pathMatch: 'full' }
+  // { path: '', component: ShoppingCartComponent },
+  // { path: 'envio', loadChildren: () => import('./tramitar-compra/envio/envio.module').then(m => m.EnvioModule) },
+  // { path: 'datos-personales', loadChildren: () => import('./tramitar-compra/datos-personales/datos-personales.module').then(m => m.DatosPersonalesModule) },
+  // { path: 'pago', loadChildren: () => import('./tramitar-compra/pago/pago.module').then(m => m.PagoModule) },
+  // { path: 'confirmacion', loadChildren: () => import('./tramitar-compra/confirmacion/confirmacion.module').then(m => m.ConfirmacionModule) },
+  // { path: '**', redirectTo: '', pathMatch: 'full' }
+
+  {
+    path: '',
+    children: [
+      { path: 'envio', component: EnvioComponent},
+      { path: 'datos-personales', component: DatosPersonalesComponent},
+      { path: 'pago', component: PagoComponent},
+      { path: 'confirmacion', component: ConfirmacionComponent},
+      { path: '', component: ShoppingCartComponent},
+      { path: '**', redirectTo: '', pathMatch: 'full' }
+    ]
+  }
 
 ];
 
--- src/app/pages/shopping-cart/shopping-cart.module.ts
+++ src/app/pages/shopping-cart/shopping-cart.module.ts
@@ -5,10 +5,20 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ShoppingCartRoutingModule } from './shopping-cart-routing.module'
 
 import { ShoppingCartComponent } from './shopping-cart.component';
+import { EnvioComponent } from './tramitar-compra/envio/envio.component';
+import { DatosPersonalesComponent } from './tramitar-compra/datos-personales/datos-personales.component';
+import { PagoComponent } from './tramitar-compra/pago/pago.component';
+import { ConfirmacionComponent } from './tramitar-compra/confirmacion/confirmacion.component';
+import { ResumenCompraComponent } from './tramitar-compra/components/resumen-compra/resumen-compra.component';
 
 @NgModule({
   declarations: [
-    ShoppingCartComponent
+    ShoppingCartComponent,
+    EnvioComponent,
+    DatosPersonalesComponent,
+    PagoComponent,
+    ConfirmacionComponent,
+    ResumenCompraComponent
   ],
   imports: [
     CommonModule,
